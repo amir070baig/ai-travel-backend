99e0760e21565a4bfaf6252213416c3dc9694af0
AmirTechDev <amirbaig070@gmail.com>
2026-05-07 19:52:29 +0530

    fix ai generate route

--- src/modules/ai/ai.controller.ts
+++ src/modules/ai/ai.controller.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from "express";
 import { generateItinerary } from "./ai.service";
-import { saveItinerary } from "./ai.service";
 
 export const generate = async (req: Request, res: Response) => {
   try {
     const content = await generateItinerary(req.body);
 
-    const userId = (req as any).user.userId;
-
     res.json({
-      content, // only return content
+      content,
     });
 
-    // res.json({ itinerary });
   } catch (err) {
-    res.status(500).json({ message: "AI error" });
+    console.error("AI ERROR:", err);
+
+    res.status(500).json({
+      message: "AI error",
+    });
   }
 };
