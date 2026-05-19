af2954afa0e5a15910199fcbe401968b558b1edd
AmirTechDev <amirbaig070@gmail.com>
2026-05-19 23:11:48 +0530

    fix cookie auth middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,26 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-export const authMiddleware = (req: any, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+export const authMiddleware = (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
 
-  if (!authHeader) {
-    return res.status(401).json({ message: "No token provided" });
-  }
-
-  // const token = authHeader.split(" ")[1];
+  // GET TOKEN FROM COOKIE
   const token = req.cookies.token;
 
+  if (!token) {
+    return res.status(401).json({
+      message: "No token provided",
+    });
+  }
+
   try {
+
     const decoded = jwt.verify(
       token,
       process.env.JWT_SECRET as string
     ) as any;
 
-    req.user = decoded; // { userId: ... }
+    req.user = decoded;
 
     next();
+
   } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
+
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+
   }
 };
