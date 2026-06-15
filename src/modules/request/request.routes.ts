83c4f28c6075f00d626ab3deea99f596cd541f03
AmirTechDev <amirbaig070@gmail.com>
2026-06-15 17:13:29 +0530

    added delete button backend

--- src/modules/request/request.routes.ts
+++ src/modules/request/request.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { submitRequest } from "./request.controller";
+import { deleteItineraryController, submitRequest } from "./request.controller";
 import { getAllRequests } from "./request.controller";
 import { acceptRevision } from "./request.controller";
 import { authMiddleware } from "../../middleware/auth.middleware";
 import { rejectRevision } from "./request.controller";
 import {  sendMessage,  getMessages,} from "./request.controller";
+
 
 const router = Router();
 
@@ -14,5 +15,6 @@ router.post("/accept", acceptRevision);
 router.post("/reject-revision", authMiddleware, rejectRevision);
 router.post(  "/message",  authMiddleware,  sendMessage);
 router.get(  "/:requestId/messages",  authMiddleware,  getMessages);
+router.delete("/delete/:id", authMiddleware, deleteItineraryController);
 
 export default router;
